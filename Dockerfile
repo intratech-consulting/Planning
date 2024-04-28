d25018ec95043b047615f940d5b4728339ce5df8
wapitie <axel.overath@student.ehb.be>
2024-04-28 15:53:35 +0200

    .env file & DB
    Description: i modified the code so it uses a .env file and now the code has a connection to the database file.
    
    Reviewed-by: Axel

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Python image as a base
+# Use a specific Python version
+FROM python:3.9
+
+# Set the working directory inside the container
+WORKDIR /TestServer/planning/github/Planning
+
+# Copy the Python script and requirements file into the container
+COPY . .
+
+# Install the required Python packages
+RUN pip install --no-cache-dir \
+    pika \
+    google-api-python-client google-auth-httplib2 google-auth-oauthlib \
+    lxml \
+    mysql-connector-python \
+    python-dotenv
+
+# Run the Python script when the container starts
+CMD [ "python3", "heartbeat_planning.py" ]
--- calendar_events.py
+++ calendar_events.py
@@ -3,9 +3,28 @@ from google.oauth2 import service_account
 import datetime
 import os.path
 import xml.etree.ElementTree as ET
+import mysql.connector
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get MySQL credentials
+mysql_host = os.getenv("MYSQL_HOST")
+mysql_user = os.getenv("MYSQL_USER")
+mysql_password = os.getenv("MYSQL_PASSWORD")
+mysql_database = os.getenv("MYSQL_DATABASE")
+
+# Establish a connection to your MySQL database
+mydb = mysql.connector.connect(
+    host=mysql_host,
+    user=mysql_user,
+    password=mysql_password,
+    database=mysql_database
+)
 
 # Email of the service account
-SERVICE_ACCOUNT_EMAIL = 'google-calendar@integration-project-420318.iam.gserviceaccount.com'
+SERVICE_ACCOUNT_EMAIL = os.getenv("SERVICE_ACCOUNT_EMAIL")
 
 # If modifying these scopes, specify the required scopes for accessing Google Calendar.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -16,17 +35,17 @@ def create_calendar():
     creds = service_account.Credentials.from_service_account_info(
         {
             "type": "service_account",
-            "project_id": "integration-project-420318",
-            "private_key_id": "d60076a099a514387563237ae1f4b8f6754dca0b",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC4cixiVVFjK4yE\nASdCVu12zM9m6I6XadAl8Gj0Wwt2hGUL0nidvuEEn3UFCdMvv3DqjcztCvqVR6K6\nE4WA8LOS3YR2GBS2/gwpYguk3/DUotCt70QcnML4bgnklsMYky03qbw7PihNIah+\n6BDdE/vznGAGXtW6Z+LBkOf/gEx198OgvsPjtuv+JBhNSG4yqPxb29Xl5wWlts0q\n01FMxa3ONRQp+dEe+FdHI60n3a/GZxG6PAjpHGnjPIPsF7iUcgETMmWT4VkhaICJ\ncDd++74BAr0t28NAwvbe31klLosBWh0tvfCaV4GTSCvVvFExbcANQ5Yoz9Mzq0OB\n5guNr67vAgMBAAECggEABMGc92Qv1xjWZMkK1CEduoSJjKPOmYT3AyNx8WsvnnzH\nhkiFJ7w2XUInaFvagE61gLbrY+l2kMbqIVZDiRGr78uakNkKH7XpZl/K4RONSE5y\nhym2Xe6E8JcV1jk1Crsw3VCZNAXkUCjgCXW+iZTsWFnd+AwWAtqO9GLiOKp8TkZs\nGDrnz1PnR/pLificKT9niJcQOi0ryYHfkFV5V6uLMr0DIXWm+/411XX8XHm49JYp\nzKgz7zJR0qHmLo4UVUROOocYCVRoKcG/WOTZl9aSOoKxtdbQIHbFl+wiRjVBH8I7\negatFJ97UvcijVHdEJQL4M+9EnqVBW4PZp+V5wJDcQKBgQDvrLrtY2zgEInxIpoW\nm5Uz5ReucFgOaz2tvLkZQRdu2Zfz3HQnBqWcACTKx1VskVqzLOCt0HxDIM0tkgZo\niDhvOoCyma4X/Rkzug1b8x9FX1G3f2VD3lc7MYfQhTA2+rPm2ZeRACX0cvB/cQeG\nmZTK1nsROLcrwPywM7g3dvMc/QKBgQDFAmfO5E8ayoXtkHPZ4kbdXnBp7Bs9twph\ncZSpyOVAuwUPkZG3LQvdj7uniIKXr5O7vTlVu+G9L4rIf5hMNVScBVToiW62Dx3A\nI+G5NTCvABMltt3Pxyt/SH9+k1cyPn27SdN/hOa6gJ0fwHCRRF2Vpu+eWny0xr2Y\n9UNCRpQ1WwKBgBIHimBFNHF+jQHounKs7YJj324t4VHkW1VwdvQhY2PhVn7vvIUb\nCOY9VIc0i4aJQKtt2tD4eACMvSKKc5T1ellTq07EJMaFMZokitC570ZGr10AZlu+\npA094pubCU95eIMLJ02vFyvWYaUUmZ5Ue07u538aJx1RfTC8E0WI2yz5AoGATexv\npvPDHjOw2ZBjYnEP2ot6J0tOUxDNOFG2nZlqpDnU/J11Z2CX6uDpNpDWJq053+4u\neWzBExV07W0ajjoh5zxsozWI5tX/zHnk1F7njLCY4jf8cCy1VbkVRLl+qc7x6FyB\nHVUx9d7dRyvU2oCSC5ll+dqMrOxXHeMU4WgNQzUCgYEAo2ISUJqFbwOPy5o/2x5K\nKLdS0iz9qhwzjm0uF2GvpNJC15LzH/e1S/hu9hghS1v76M7IhWml0WcgTOExTxb1\nx6fMlUcfNR5MmSVn1itZizAZieldGZ7DknotfiWXbQ6WpZrJloZ9WG2Z9OG+8XoO\nodPZBjIQ/yw3Y/qknZFdVj4=\n-----END PRIVATE KEY-----\n",
-            "client_email": "caledars@integration-project-420318.iam.gserviceaccount.com",
-            "client_id": "110225367326596907132",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/caledars%40integration-project-420318.iam.gserviceaccount.com",
-            "universe_domain": "googleapis.com"
-            },
+            "project_id": os.getenv("PROJECT_ID"),
+            "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+            "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+            "client_email": os.getenv("CLIENT_EMAIL"),
+            "client_id": os.getenv("CLIENT_ID"),
+            "auth_uri": os.getenv("AUTH_URI"),
+            "token_uri": os.getenv("TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+            "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+            "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+        },
         scopes=SCOPES
     )
 
@@ -47,6 +66,14 @@ def create_calendar():
 
     created_calendar = service.calendars().insert(body=calendar).execute()
     print('Calendar created:', created_calendar['id'])
+
+    # Save the calendarID to MySQL database
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO calendar_table (calendar_id) VALUES (%s)"
+    val = (created_calendar['id'],)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
 
     # Share the calendar publicly
     rule = {
