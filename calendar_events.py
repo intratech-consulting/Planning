8d24651472aa6cabce8c18acdf60ebf06c57065b
wapitie <axel.overath@student.ehb.be>
2024-05-06 19:16:53 +0200

    Update calendar_events.py

--- calendar_events.py
+++ calendar_events.py
@@ -153,10 +153,10 @@ def add_event_from_database(event_id, calendar_service, calendar_id, mysql_conne
             event_body = {
                 'summary': event_details[1],  # Assuming summary is at index 1 in the database
                 'start': {
-                    'dateTime': event_details[2].isoformat(),  # Assuming start datetime is at index 2
+                    'dateTime': event_details[2].isoformat()+ 'Z',  # Assuming start datetime is at index 2
                 },
                 'end': {
-                    'dateTime': event_details[3].isoformat(),  # Assuming end datetime is at index 3
+                    'dateTime': event_details[3].isoformat()+ 'Z',  # Assuming end datetime is at index 3
                 },
                 'timeZone': 'Europe/Brussels',
                 'location': event_details[4],  # Assuming location is at index 4
