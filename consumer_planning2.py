0f3214be4cb7c27315a5842762d432982d3cf739
petritg <petrit.gashi@student.ehb.be>
2024-05-12 21:30:45 +0200

    added port

--- consumer_planning2.py
+++ consumer_planning2.py
@@ -295,6 +295,7 @@ def get_database_connection():
     try: 
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT', 3306),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
             database=os.getenv('DB_DATABASE')
