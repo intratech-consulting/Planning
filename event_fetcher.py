799750270994362210b03129bd81db1e9eb6814c
wapitie <axel.overath@student.ehb.be>
2024-05-20 17:08:29 +0200

    Update event_fetcher.py

--- event_fetcher.py
+++ event_fetcher.py
@@ -45,21 +45,15 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 # Load environment variables from .env file
 load_dotenv()
 
-# MySQL Database Connection
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_DATABASE = os.getenv("DB_DATABASE")
-
-
 async def connect_to_mysql():
     try:
         # Connect to MySQL
         connection = await aiomysql.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_DATABASE
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT', 3306),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_DATABASE')
         )
         logging.info("Connected to MySQL database")
         return connection
